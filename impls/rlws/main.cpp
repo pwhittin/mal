972489e8d71e5d544e0c96aaef2a4db13b144990
Paul Whittington <pwhittington@ricelake.com>
2022-07-28 15:32:42 -0600

    passes all tests

--- impls/rlws/main.cpp
+++ impls/rlws/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using S = std::string;
+
+auto READ = [](const auto& s) { return s; };
+auto EVAL = [](const auto& s) { return s; };
+auto PRINT = [](const auto& s) { return s; };
+auto rep = [](const auto& s) { return PRINT(EVAL(READ(s))); };
+
+static const auto PROMPT{S{"user> "}};
+
+const auto ReadLine = [](auto& in) noexcept
+{
+    auto s{S{""}};
+    std::getline(in, s);
+    return s;
+};
+
+const auto IsEOF = [](const auto& in) noexcept { return in.eof(); };
+
+int main()
+{
+    while (not IsEOF(std::cin))
+    {
+        std::cout << PROMPT;
+        std::cout << rep(ReadLine(std::cin)) << "\n";
+    }
+
     return 0;
 }
