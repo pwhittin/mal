a615bd2faa8a3aac07ba3f5449e25ba850067275
Paul Whittington <pwhittington@ricelake.com>
2022-08-03 10:16:14 -0600

    refactor for readability

--- impls/rlws/printer.hpp
+++ impls/rlws/printer.hpp
@@ -18,8 +18,9 @@ static const auto SequenceTokens = [](const auto type)
         return Answer(types::MAP_TOKEN_START, types::MAP_TOKEN_END);
     case types::RLWSTypes::RLWS_VECTOR:
         return Answer(types::VECTOR_TOKEN_START, types::VECTOR_TOKEN_END);
+    default:
+        return Answer("", "");
     }
-    return Answer("", ""); // quiets compiler warning
 };
 
 static const auto ExpandGeneric = [](const types::S& s, const types::S& expandSequence, const types::S& replaceSequence)
@@ -44,42 +45,49 @@ static const auto ExpandGeneric = [](const types::S& s, const types::S& expandSe
 static const auto ExpandDoubleQuote = [](const auto& s) { return ExpandGeneric(s, "\"", "\\\""); };
 static const auto ExpandNewline = [](const auto& s) { return ExpandGeneric(s, "\n", "\\n"); };
 static const auto ExpandBackslash = [](const auto& s) { return ExpandGeneric(s, "\\", "\\\\"); };
-
 static const auto ExpandString = [](const auto& s) { return ExpandDoubleQuote(ExpandNewline(ExpandBackslash(s))); };
+
+auto PrintAnInteger = [](const auto& rlwsType) { return std::to_string(std::get<types::I>(rlwsType.value)); };
+auto PrintAString = [](const auto& rlwsType) { return "\"" + ExpandString(std::get<types::S>(rlwsType.value)) + "\""; };
+auto PrintASymbol = [](const auto& rlwsType) { return std::get<types::S>(rlwsType.value); };
+
+static types::S PrintString(const types::RLWSType& rlwsType);
+auto PrintASequence = [](const auto& rlwsType)
+{
+    auto [startToken, endToken]{SequenceTokens(rlwsType.type)};
+    auto sequence{std::get<types::L>(rlwsType.value)};
+    auto result{types::S{startToken}};
+    for (const auto& v : sequence)
+        result += PrintString(v) + " ";
+    if (' ' == result.back())
+        result.back() = endToken[0];
+    else
+        result += endToken;
+    return result;
+};
+
+static types::S PrintString(const types::RLWSType& rlwsType)
+{
+    switch (rlwsType.type)
+    {
+    case types::RLWSTypes::RLWS_INTEGER:
+        return PrintAnInteger(rlwsType);
+    case types::RLWSTypes::RLWS_STRING:
+        return PrintAString(rlwsType);
+    case types::RLWSTypes::RLWS_SYMBOL:
+        return PrintASymbol(rlwsType);
+    case types::RLWSTypes::RLWS_LIST:
+    case types::RLWSTypes::RLWS_MAP:
+    case types::RLWSTypes::RLWS_VECTOR:
+        return PrintASequence(rlwsType);
+    }
+    return ""; // quite compiler warning
+}
 
 namespace printer
 {
 
-types::S PrintStr(const types::RLWSType& rlwsType)
-{
-    auto result{types::S{""}};
-    switch (rlwsType.type)
-    {
-    case types::RLWSTypes::RLWS_INTEGER:
-        result = std::to_string(std::get<types::I>(rlwsType.value));
-        break;
-    case types::RLWSTypes::RLWS_STRING:
-        result = "\"" + ExpandString(std::get<types::S>(rlwsType.value)) + "\"";
-        break;
-    case types::RLWSTypes::RLWS_SYMBOL:
-        result = std::get<types::S>(rlwsType.value);
-        break;
-    case types::RLWSTypes::RLWS_LIST:
-    case types::RLWSTypes::RLWS_MAP:
-    case types::RLWSTypes::RLWS_VECTOR:
-        auto [startToken, endToken]{SequenceTokens(rlwsType.type)};
-        auto sequence{std::get<types::L>(rlwsType.value)};
-        result += startToken;
-        for (const auto& v : sequence)
-            result += PrintStr(v) + " ";
-        if (' ' == *result.rbegin())
-            *result.rbegin() = endToken[0];
-        else
-            result += endToken;
-        break;
-    }
-    return result;
-}
+auto PrintStr = [](const auto& rlwsType) { return PrintString(rlwsType); };
 
 } // namespace printer
 
