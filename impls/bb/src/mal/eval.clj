573ac619b9cb1f56bee9a15c208c6dd4126bf870
Paul Whittington <pwhittington@ricelake.com>
2022-08-18 15:08:21 -0600

    all unit tests passing

--- impls/bb/src/mal/eval.clj
+++ impls/bb/src/mal/eval.clj
@@ -15,20 +15,47 @@
     :mal-vector (eval-sequence :mal-vector mal-value env)
     mal))
 
-(defn eval-mal-def! [[[symbol-type symbol-value :as symbol] value & others :as args] env]
+(defn eval-mal-def!-validate! [[[symbol-type symbol-value :as symbol] value & others :as args] env]
   (when (zero? (count args))
     (throw (Exception. "def!: wrong number of args (0)")))
   (when (not= :mal-symbol symbol-type)
-    (throw (Exception. (str "def!: first arg is not a symbol " (p/print-str symbol)))))
+    (throw (Exception. (str "def!: first arg is not a symbol: " (p/print-str symbol)))))
   (when (not value)
     (throw (Exception. (str "def!: '" (p/print-str symbol) "' wrong number of args (1)"))))
   (when (seq others)
-    (throw (Exception. (str "def!: '" (p/print-str symbol) "' wrong number of args (" (count args) ") "
-                            (pr-str (mapv p/print-str args))))))
-  (en/set env symbol (eval value env))
-  symbol)
+    (throw (Exception. (str "def!: '" (p/print-str symbol) "' wrong number of args (" (count args) "): "
+                            (pr-str (mapv p/print-str args)))))))
 
-(defn eval-mal-let* [mal env])
+(defn eval-mal-def! [[[symbol-type symbol-value :as symbol] value & others :as args] env]
+  (eval-mal-def!-validate! args env)
+  (let [v (eval value env)]
+    (en/set env symbol v)
+    v))
+
+(defn eval-mal-let*-validate! [[[sequence-type sequence-value :as sequence] value & others :as args] env]
+  (when (zero? (count args))
+    (throw (Exception. "let*: wrong number of args (0)")))
+  (when (not (#{:mal-list :mal-vector} sequence-type))
+    (throw (Exception. (str "let*: first arg is not a list or vector: " (p/print-str sequence)))))
+  (when (not value)
+    (throw (Exception. (str "let*: wrong number of args (1)"))))
+  (when (seq others)
+    (throw (Exception. (str "let*: wrong number of args (" (count args) "): " (pr-str (mapv p/print-str args))))))
+  (when (odd? (count sequence-value))
+    (throw (Exception. (str "let*: bindings sequence odd forms count (" (count sequence-value) "): "
+                            (pr-str (mapv p/print-str sequence-value))))))
+  (when (some (fn [[type _]] (not= :mal-symbol type)) (take-nth 2 sequence-value))
+    (throw (Exception. (str "let*: non-symbol binding: " (pr-str (mapv p/print-str sequence-value)))))))
+
+(defn add-bindings! [env bindings]
+  (doseq [[symbol value] (partition 2 bindings)]
+    (en/set env symbol (eval value env))))
+
+(defn eval-mal-let* [[[sequence-type sequence-value :as sequence] value & others :as args] env]
+  (eval-mal-let*-validate! args env)
+  (let [let-env (en/create env)]
+    (add-bindings! let-env sequence-value)
+    (eval value let-env)))
 
 (defn eval-fn [mal-value env]
   (let [[_ [[fn-type fn-value] & args]] (eval-ast mal-value env)]
@@ -39,37 +66,30 @@
 (def mal-def! [:mal-symbol "def!"])
 (def mal-let* [:mal-symbol "let*"])
 
-(defn eval-list [[first-mal-value & rest-mal-value :as mal-value] env]
+(defn eval-list [[_ [first-mal-value & rest-mal-value :as mal-value] :as mal] env]
   (condp = first-mal-value
     mal-def! (eval-mal-def! rest-mal-value env)
     mal-let* (eval-mal-let* rest-mal-value env)
-    :else (eval-fn mal-value env)))
+    (eval-fn mal env)))
 
 (defn eval [[mal-type mal-value :as mal] env]
   (if (not= :mal-list mal-type)
     (eval-ast mal env)
     (if (empty? mal-value)
       mal
-      (eval-list mal-value env))))
+      (eval-list mal env))))
 
 (comment
 
-  (defn doit [x]
-    (condp = x
-      mal-def! "def!"
-      mal-let* "let*"
-      :else "fn"))
+  (def e (en/create nil))
+  (def bindings [[:mal-symbol "a"] [:mal-integer 1] [:mal-symbol "b"] [:mal-integer 2]])
 
-  (doit [:mal-symbol "def!"])
+  (doseq [[symbol value] (partition 2 bindings)]
+    (en/set e symbol value))
 
-  (doit [1 2])
+  (prn @e)
 
-  (eval [:mal-symbol "abc"] {"abc" [:mal-fn "fn"]})
-  (eval [:mal-integer 1] {"abc" [:mal-fn "fn"]})
-  (eval [:mal-string "fred"] {"abc" [:mal-fn "fn"]})
-  (map #(eval % {"abc" [:mal-fn "fn"]}) [[:mal-symbol "abc"] [:mal-integer 1] [:mal-string "fred"]])
+  (partition 2 ['a 1 'b 2])
 
-  (eval [:mal-list [[:mal-symbol "abc"] [:mal-integer 1] [:mal-string "fred"]]] {"abc" [:mal-fn "fn"]})
-
-  ;
+;
   )
