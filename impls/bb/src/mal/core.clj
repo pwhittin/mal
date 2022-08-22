066e5af2abf6ef0938a28a024a5d8eefeeddbb99
Paul Whittington <pwhittington@ricelake.com>
2022-08-22 12:05:19 -0600

    < working

--- impls/bb/src/mal/core.clj
+++ impls/bb/src/mal/core.clj
@@ -13,12 +13,13 @@
     [:mal-integer (apply + (map second ints))]))
 
 (defn mal-count [[[first-mal-type first-mal-value :as first-mal] & rest-mals :as mals]]
+  (when (nil? first-mal-type)
+    (throw (Exception. "count: wrong number of arguments (0)")))
   (when (seq rest-mals)
     (throw (Exception.
             (str "count: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
   (when (not (#{:mal-list :mal-map :mal-nil :mal-vector} first-mal-type))
-    (throw (Exception.
-            (str "empty?: argument must be a list, map, nil or vector '" (p/print-str first-mal) "'"))))
+    (throw (Exception. (str "count: argument must be a list, map, nil or vector '" (p/print-str first-mal) "'"))))
   [:mal-integer (if (= :mal-nil first-mal-type)
                   0
                   (/ (count first-mal-value) (if (= :mal-map first-mal-type) 2 1)))])
@@ -30,13 +31,25 @@
     [:mal-integer (int (apply / (map second ints)))]))
 
 (defn mal-empty? [[[first-mal-type first-mal-value :as first-mal] & rest-mals :as mals]]
+  (when (nil? first-mal-type)
+    (throw (Exception. "empty?: wrong number of arguments (0)")))
   (when (seq rest-mals)
     (throw (Exception.
             (str "empty?: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
   (when (not (#{:mal-list :mal-map :mal-vector} first-mal-type))
-    (throw (Exception.
-            (str "empty?: argument must be a list, map or vector '" (p/print-str first-mal) "'"))))
+    (throw (Exception. (str "empty?: argument must be a list, map or vector '" (p/print-str first-mal) "'"))))
   (if (zero? (count first-mal-value)) [:mal-true true] [:mal-false false]))
+
+(defn mal-less-than [[[mal-type-1 mal-value-1] [mal-type-2 mal-value-2] & rest-mals :as mals]]
+  (when (nil? mal-type-1)
+    (throw (Exception. "<: wrong number of arguments (0)")))
+  (when (nil? mal-type-2)
+    (throw (Exception. "<: wrong number of arguments (1) '" (p/print-str [:mal-list mals]) "'")))
+  (when (seq rest-mals)
+    (throw (Exception. (str "<: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
+  (when (not (= :mal-integer mal-type-1 mal-type-2))
+    (throw (Exception. (str "<: arguments must be integers '" (p/print-str [:mal-list mals]) "'"))))
+  (if (< mal-value-1 mal-value-2) [:mal-true true] [:mal-false false]))
 
 (defn mal-list [mals]
   [:mal-list mals])
@@ -62,6 +75,7 @@
    [:mal-symbol "/"] [:mal-fn mal-divide]
    [:mal-symbol "*"] [:mal-fn mal-multiply]
    [:mal-symbol "-"] [:mal-fn mal-subtract]
+   [:mal-symbol "<"] [:mal-fn mal-less-than]
    [:mal-symbol "count"] [:mal-fn mal-count]
    [:mal-symbol "empty?"] [:mal-fn mal-empty?]
    [:mal-symbol "list"] [:mal-fn mal-list]
