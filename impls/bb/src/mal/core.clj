ff20aa8a8d8dbedd53fcdda45f1bba9ea9320ed0
Paul Whittington <pwhittington@ricelake.com>
2022-08-22 15:02:51 -0600

    = working

--- impls/bb/src/mal/core.clj
+++ impls/bb/src/mal/core.clj
@@ -8,6 +8,27 @@
               (str fn-symbol ": not all arguments are integers '" (p/print-str [:mal-list non-integer-mals]) "'")))))
   mals)
 
+(defn mal-integer-compare [fn-str fn [[mal-type-1 mal-value-1] [mal-type-2 mal-value-2] & rest-mals :as mals]]
+  (when (nil? mal-type-1)
+    (throw (Exception. (str fn-str ": wrong number of arguments (0)"))))
+  (when (nil? mal-type-2)
+    (throw (Exception. (str fn-str ": wrong number of arguments (1) '" (p/print-str [:mal-list mals]) "'"))))
+  (when (seq rest-mals)
+    (throw (Exception.
+            (str fn-str ": wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
+  (when (not (= :mal-integer mal-type-1 mal-type-2))
+    (throw (Exception. (str fn-str ": arguments must be integers '" (p/print-str [:mal-list mals]) "'"))))
+  (if (fn mal-value-1 mal-value-2) [:mal-true true] [:mal-false false]))
+
+(defn is-list-or-vector? [[sequence-type _]]
+  (#{:mal-list :mal-vector} sequence-type))
+
+(defn is-map? [[sequence-type _]]
+  (= :mal-map sequence-type))
+
+(defn is-sequence? [[sequence-type _]]
+  (#{:mal-list :mal-map :mal-vector} sequence-type))
+
 (defn mal-add [mals]
   (let [ints (all-integers?! "+" mals)]
     [:mal-integer (apply + (map second ints))]))
@@ -18,7 +39,7 @@
   (when (seq rest-mals)
     (throw (Exception.
             (str "count: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
-  (when (not (#{:mal-list :mal-map :mal-nil :mal-vector} mal-type-1))
+  (when (not (is-sequence? mal-1))
     (throw (Exception. (str "count: argument must be a list, map, nil or vector '" (p/print-str mal-1) "'"))))
   [:mal-integer (if (= :mal-nil mal-type-1)
                   0
@@ -36,21 +57,30 @@
   (when (seq rest-mals)
     (throw (Exception.
             (str "empty?: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
-  (when (not (#{:mal-list :mal-map :mal-vector} mal-type-1))
+  (when (not (is-sequence? mal-1))
     (throw (Exception. (str "empty?: argument must be a list, map or vector '" (p/print-str mal-1) "'"))))
   (if (zero? (count mal-value-1)) [:mal-true true] [:mal-false false]))
 
-(defn mal-integer-compare [fn-str fn [[mal-type-1 mal-value-1] [mal-type-2 mal-value-2] & rest-mals :as mals]]
+(defn mal-equal [[[mal-type-1 mal-value-1 :as mal-1] [mal-type-2 mal-value-2 :as mal-2] & rest-mals :as mals]]
   (when (nil? mal-type-1)
-    (throw (Exception. (str fn-str ": wrong number of arguments (0)"))))
+    (throw (Exception. "=: wrong number of arguments (0)")))
   (when (nil? mal-type-2)
-    (throw (Exception. (str fn-str ": wrong number of arguments (1) '" (p/print-str [:mal-list mals]) "'"))))
+    (throw (Exception. (str "=: wrong number of arguments (1) '" (p/print-str [:mal-list mals]) "'"))))
   (when (seq rest-mals)
     (throw (Exception.
-            (str fn-str ": wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
-  (when (not (= :mal-integer mal-type-1 mal-type-2))
-    (throw (Exception. (str fn-str ": arguments must be integers '" (p/print-str [:mal-list mals]) "'"))))
-  (if (fn mal-value-1 mal-value-2) [:mal-true true] [:mal-false false]))
+            (str "=: wrong number of arguments (" (count mals) ") '" (p/print-str [:mal-list mals]) "'"))))
+  (cond
+    (and (is-list-or-vector? mal-1) (is-list-or-vector? mal-2))
+    (if (and (= (count mal-value-1) (count mal-value-2))
+             (empty? (filter #(= [:mal-false false] %) (map #(mal-equal [%1 %2]) mal-value-1 mal-value-2))))
+      [:mal-true true]
+      [:mal-false false])
+
+    (and (is-map? mal-1) (is-map? mal-2))
+    (if (= (apply hash-map mal-value-1) (apply hash-map mal-value-2)) [:mal-true true] [:mal-false false])
+
+    :else
+    (if (= mal-1 mal-2) [:mal-true true] [:mal-false false])))
 
 (def mal-less-than (partial mal-integer-compare "<" <))
 (def mal-less-than-or-equal (partial mal-integer-compare "<=" <=))
@@ -83,6 +113,7 @@
    [:mal-symbol "/"] [:mal-fn mal-divide]
    [:mal-symbol "*"] [:mal-fn mal-multiply]
    [:mal-symbol "-"] [:mal-fn mal-subtract]
+   [:mal-symbol "="] [:mal-fn mal-equal]
    [:mal-symbol "<"] [:mal-fn mal-less-than]
    [:mal-symbol "<="] [:mal-fn mal-less-than-or-equal]
    [:mal-symbol ">"] [:mal-fn mal-greater-than]
